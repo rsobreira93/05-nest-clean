14f1ed107138b1e1c761caf1639f3d93c965ba67
Romulo Sobreira <sobreiraromuloes@gmail.com>
2023-09-23 13:51:19 -0300

    feat: add new envs

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,15 +3,21 @@ import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigService } from '@nestjs/config';
 import { Env } from 'src/env';
+
 @Module({
   imports: [
     PassportModule,
     JwtModule.registerAsync({
       inject: [ConfigService],
+      global: true,
       useFactory(config: ConfigService<Env, true>) {
-        const secret = config.get('JWT_SECRET', { infer: true });
+        const privateKey = config.get('JWT_PRIVATE_KEY', { infer: true });
+        const publicKey = config.get('JWT_PUBLIC_KEY', { infer: true });
+
         return {
-          secret,
+          signOptions: { algorithm: 'RS256' },
+          privateKey: Buffer.from(privateKey, 'base64'),
+          publicKey: Buffer.from(publicKey, 'base64'),
         };
       },
     }),
