e532f8dfd230549cd25b63c8a02b07596b81c5b0
Romulo Sobreira <sobreiraromuloes@gmail.com>
2023-09-24 10:47:45 -0300

    refactor: adjust import to @/

--- src/controllers/fetch-recent-questions.controller.ts
+++ src/controllers/fetch-recent-questions.controller.ts
@@ -1,10 +1,10 @@
 import { Controller, Get, Query, UseGuards, UsePipes } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 
-import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
-import { ZodValidationPipe } from 'src/pipes/zod-validation-pipe';
+import { JwtAuthGuard } from '@/auth/jwt-auth.guard';
+import { ZodValidationPipe } from '@/pipes/zod-validation-pipe';
 
-import { PrismaService } from 'src/prisma/prisma.service';
+import { PrismaService } from '@/prisma/prisma.service';
 import { z } from 'zod';
 
 const pageQueryParamSchema = z
