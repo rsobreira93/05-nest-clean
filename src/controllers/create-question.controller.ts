b372d86bdeb0dbdb3e39e7955e7dda8ecf0369d8
Romulo Sobreira <sobreiraromuloes@gmail.com>
2023-09-24 09:42:08 -0300

    feat: add validation on create question

--- src/controllers/create-question.controller.ts
+++ src/controllers/create-question.controller.ts
@@ -4,7 +4,18 @@ import { CurrentUser } from 'src/auth/current-user-decorator';
 
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 import { userPayload } from 'src/auth/jwt.strategy';
+import { ZodValidationPipe } from 'src/pipes/zod-validation-pipe';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { z } from 'zod';
+
+const createQuestionBodySchema = z.object({
+  title: z.string(),
+  context: z.string(),
+});
+
+const bodyValidationPipe = new ZodValidationPipe(createQuestionBodySchema);
+
+type CreateQuestionBodySchema = z.infer<typeof createQuestionBodySchema>;
 
 @Controller('/questions')
 @UseGuards(JwtAuthGuard)
@@ -13,7 +24,32 @@ export class CreateQuestion {
 
   @Post()
   @UsePipes()
-  async handle(@CurrentUser() user: userPayload) {
-    return user;
+  async handle(
+    @CurrentUser() user: userPayload,
+    @Body(bodyValidationPipe)
+    body: CreateQuestionBodySchema,
+  ) {
+    const { title, context } = body;
+    const { sub: authorId } = user;
+
+    const slug = this.convertToSlug(title);
+
+    await this.prismaService.question.create({
+      data: {
+        title,
+        content: context,
+        authorId,
+        slug,
+      },
+    });
+  }
+
+  private convertToSlug(title: string): string {
+    return title
+      .toLowerCase()
+      .normalize('NFD')
+      .replace(/[\u0300-\u036f]/g, '')
+      .replace(/[^\w\s-]/g, '')
+      .replace(/\s+/g, '-');
   }
 }
