b2fe97862bc16bc6b8898c854aeb6f7e93dcd566
Romulo Sobreira <sobreiraromuloes@gmail.com>
2023-09-30 08:47:29 -0300

    test: tests e2e

---
+++ src/controllers/create-account.controller.e2e-spec.ts
@@ -0,0 +1,37 @@
+import { AppModule } from '@/app.module';
+import { PrismaService } from '@/prisma/prisma.service';
+import { INestApplication } from '@nestjs/common';
+import { Test } from '@nestjs/testing';
+import request from 'supertest';
+describe('Create account (e2e)', () => {
+  let app: INestApplication;
+  let prisma: PrismaService;
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleRef.createNestApplication();
+    prisma = moduleRef.get(PrismaService);
+
+    await app.init();
+  });
+  test('[POST] /accounts', async () => {
+    const response = await request(app.getHttpServer()).post('/accounts').send({
+      name: 'John Doe',
+      email: 'johndoe@email.com',
+      password: 'password',
+    });
+
+    expect(response.statusCode).toBe(201);
+
+    const userOnDataBase = await prisma.user.findUnique({
+      where: {
+        email: 'johndoe@email.com',
+      },
+    });
+
+    expect(userOnDataBase).toBeTruthy();
+  });
+});
