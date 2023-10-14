621c306c1791fbfd183e2d2c5cd28fd04e4a8a3f
Romulo Sobreira <sobreiraromuloes@gmail.com>
2023-10-14 14:29:18 -0300

    feat: add new fields on student entity

--- src/domain/forum/enterprise/entities/student.ts
+++ src/domain/forum/enterprise/entities/student.ts
@@ -3,9 +3,21 @@ import { UniqueEntityId } from '@/core/entities/unique-entity-id';
 
 interface StudentProps {
   name: string;
+  email: string;
+  password: string;
 }
 
 export class Student extends Entity<StudentProps> {
+  get name(): string {
+    return this.props.name;
+  }
+  get email(): string {
+    return this.props.email;
+  }
+  get password(): string {
+    return this.props.password;
+  }
+
   static create(props: StudentProps, id?: UniqueEntityId) {
     const student = new Student(
       {
