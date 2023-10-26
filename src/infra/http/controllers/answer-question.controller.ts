f5cfa7cb11865b4bbbf0744fbfd594418881e29c
Romulo Sobreira <sobreiraromuloes@gmail.com>
2023-10-26 19:25:25 -0300

    feat: add answer question controller

---
+++ src/infra/http/controllers/answer-question.controller.ts
@@ -0,0 +1,50 @@
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  Param,
+  Post,
+  UsePipes,
+} from '@nestjs/common';
+import { CurrentUser } from '@/infra/auth/current-user-decorator';
+
+import { userPayload } from '@/infra/auth/jwt.strategy';
+import { ZodValidationPipe } from '@/infra/http/pipes/zod-validation-pipe';
+import { z } from 'zod';
+import { AnswerQuestionUseCase } from '@/domain/forum/application/use-cases/answer-question';
+
+const answerQuestionBodySchema = z.object({
+  content: z.string(),
+});
+
+const bodyValidationPipe = new ZodValidationPipe(answerQuestionBodySchema);
+
+type AnswerQuestionBodySchema = z.infer<typeof answerQuestionBodySchema>;
+
+@Controller('/questions/:questionId/answers')
+export class AnswerQuestionController {
+  constructor(private answerQuestion: AnswerQuestionUseCase) {}
+
+  @Post()
+  @UsePipes()
+  async handle(
+    @CurrentUser() user: userPayload,
+    @Body(bodyValidationPipe)
+    body: AnswerQuestionBodySchema,
+    @Param('questionId') questionId: string,
+  ) {
+    const { content } = body;
+    const { sub: authorId } = user;
+
+    const result = await this.answerQuestion.execute({
+      content,
+      questionId,
+      authorId,
+      attachmentsIds: [],
+    });
+
+    if (result.isLeft()) {
+      throw new BadRequestException();
+    }
+  }
+}
