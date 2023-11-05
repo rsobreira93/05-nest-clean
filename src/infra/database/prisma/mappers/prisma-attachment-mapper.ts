a654d4cb6ab71c3d564a3fa5e6db76f5dcb2c134
Romulo Sobreira <sobreiraromuloes@gmail.com>
2023-11-05 08:57:41 -0300

    test: add prisma attachment mapper

---
+++ src/infra/database/prisma/mappers/prisma-attachment-mapper.ts
@@ -0,0 +1,14 @@
+import { Attachment } from '@/core/entities/attachment';
+import { Prisma } from '@prisma/client';
+
+export class PrismaAttachmentMapper {
+  static toPrisma(
+    attachment: Attachment,
+  ): Prisma.AttachmentUncheckedCreateInput {
+    return {
+      id: attachment.id.toString(),
+      title: attachment.title,
+      url: attachment.url,
+    };
+  }
+}
