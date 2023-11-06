fea3756e94fbb2902a57138ba61870680e23bfa5
Romulo Sobreira <sobreiraromuloes@gmail.com>
2023-11-06 20:25:48 -0300

    feat: Add answer attachments repository to

--- src/infra/database/prisma/repositories/prisma-answers-repository.ts
+++ src/infra/database/prisma/repositories/prisma-answers-repository.ts
@@ -4,10 +4,14 @@ import { Answer } from '@/domain/forum/enterprise/entities/answer';
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma.service';
 import { PrismaAnswerMapper } from '../mappers/prisma-answer-mapper';
+import { AnswerAttachmentsRepository } from '@/domain/forum/application/repositories/answer-attachments-repository';
 
 @Injectable()
 export class PrismaAnswersRepository implements AnswerRepository {
-  constructor(private prisma: PrismaService) {}
+  constructor(
+    private prisma: PrismaService,
+    private answersAttachmentsRepository: AnswerAttachmentsRepository,
+  ) {}
 
   async findById(id: string): Promise<Answer | null> {
     const answer = await this.prisma.answer.findUnique({
@@ -44,16 +48,30 @@ export class PrismaAnswersRepository implements AnswerRepository {
   async save(answer: Answer): Promise<void> {
     const data = PrismaAnswerMapper.toPrisma(answer);
 
-    await this.prisma.answer.update({
-      where: { id: data.id },
-      data,
-    });
+    await Promise.all([
+      this.prisma.answer.update({
+        where: { id: data.id },
+        data,
+      }),
+
+      this.answersAttachmentsRepository.createMany(
+        answer.attachments.getNewItems(),
+      ),
+
+      this.answersAttachmentsRepository.deleteMany(
+        answer.attachments.getRemovedItems(),
+      ),
+    ]);
   }
 
   async create(answer: Answer): Promise<void> {
     const data = PrismaAnswerMapper.toPrisma(answer);
 
     await this.prisma.answer.create({ data });
+
+    await this.answersAttachmentsRepository.createMany(
+      answer.attachments.getItems(),
+    );
   }
 
   async delete(answer: Answer): Promise<void> {
