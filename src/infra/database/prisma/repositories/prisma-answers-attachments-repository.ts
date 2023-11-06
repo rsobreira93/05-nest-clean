244fbaf41266effc4e3646bca141d3f9ff48729d
Romulo Sobreira <sobreiraromuloes@gmail.com>
2023-11-06 20:23:45 -0300

    feat: add new methods

--- src/infra/database/prisma/repositories/prisma-answers-attachments-repository.ts
+++ src/infra/database/prisma/repositories/prisma-answers-attachments-repository.ts
@@ -2,7 +2,7 @@ import { AnswerAttachment } from '@/core/entities/answer-attachment';
 import { AnswerAttachmentsRepository } from '@/domain/forum/application/repositories/answer-attachments-repository';
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma.service';
-import { PrismaAnswerAttachmentMapper } from '../mappers/prisma-ansert-attachment-mapper';
+import { PrismaAnswerAttachmentMapper } from '../mappers/prisma-answer-attachment-mapper';
 
 @Injectable()
 export class PrismaAnswersAttachmentsRepository
@@ -19,6 +19,34 @@ export class PrismaAnswersAttachmentsRepository
 
     return answersAttachments.map(PrismaAnswerAttachmentMapper.toDomain);
   }
+
+  async createMany(attachments: AnswerAttachment[]): Promise<void> {
+    if (attachments.length === 0) {
+      return;
+    }
+
+    const data = PrismaAnswerAttachmentMapper.toPrismaUpdateMany(attachments);
+
+    await this.prisma.attachment.updateMany(data);
+  }
+  async deleteMany(attachments: AnswerAttachment[]): Promise<void> {
+    if (attachments.length === 0) {
+      return;
+    }
+
+    const attachmentsId = attachments.map((attachment) => {
+      return attachment.id.toString();
+    });
+
+    await this.prisma.attachment.deleteMany({
+      where: {
+        id: {
+          in: attachmentsId,
+        },
+      },
+    });
+  }
+
   async deleteManyByAnswerId(answerId: string): Promise<void> {
     await this.prisma.attachment.deleteMany({
       where: {
