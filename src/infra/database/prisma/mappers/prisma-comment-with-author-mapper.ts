5bda3687c6b3fbf5774c8c91b8a13d4ab3bf6201
Romulo Sobreira <sobreiraromuloes@gmail.com>
2023-11-09 19:08:19 -0300

    feat: Add PrismaCommentWithAuthorMapper to map comments with their authors

---
+++ src/infra/database/prisma/mappers/prisma-comment-with-author-mapper.ts
@@ -0,0 +1,19 @@
+import { Comment as PrismaComment, User as PrismaUser } from '@prisma/client';
+import { CommentWithAuthor } from '@/domain/forum/enterprise/entities/value-objects/comment-with-author';
+import { UniqueEntityId } from '@/core/entities/unique-entity-id';
+
+type PrismaCommentWithAuthor = PrismaComment & {
+  author: PrismaUser;
+};
+
+export class PrismaCommentWithAuthorMapper {
+  static toDomain(raw: PrismaCommentWithAuthor): CommentWithAuthor {
+    return CommentWithAuthor.create({
+      commentId: new UniqueEntityId(raw.id),
+      authorId: new UniqueEntityId(raw.author.id),
+      author: raw.author.name,
+      content: raw.content,
+      createdAt: raw.createdAt,
+    });
+  }
+}
