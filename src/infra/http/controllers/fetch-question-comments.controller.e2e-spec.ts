a551d56377a2c9baca79e82e51f979bdcfdd6342
Romulo Sobreira <sobreiraromuloes@gmail.com>
2023-11-09 19:09:16 -0300

    test: Add author name to question comments in fetch controller

--- src/infra/http/controllers/fetch-question-comments.controller.e2e-spec.ts
+++ src/infra/http/controllers/fetch-question-comments.controller.e2e-spec.ts
@@ -33,7 +33,7 @@ describe('Fetch question comments (E2E)', () => {
   });
 
   test('[GET] /questions/:questionId/comments', async () => {
-    const user = await studentFactory.makePrismaStudent();
+    const user = await studentFactory.makePrismaStudent({ name: 'John Doe' });
 
     const accessToken = jwt.sign({ sub: user.id.toString() });
 
@@ -65,8 +65,14 @@ describe('Fetch question comments (E2E)', () => {
     expect(response.statusCode).toBe(200);
     expect(response.body).toEqual({
       comments: expect.arrayContaining([
-        expect.objectContaining({ content: 'Question content 1' }),
-        expect.objectContaining({ content: 'Question content 2' }),
+        expect.objectContaining({
+          content: 'Question content 1',
+          authorName: 'John Doe',
+        }),
+        expect.objectContaining({
+          content: 'Question content 2',
+          authorName: 'John Doe',
+        }),
       ]),
     });
   });
