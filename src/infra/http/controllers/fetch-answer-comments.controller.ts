f69231ce437f610b2327d950e24849e5dfdfb0e7
Romulo Sobreira <sobreiraromuloes@gmail.com>
2023-11-15 14:17:38 -0300

    refactor: Update CommentPresenter to CommentWithAuthorPresenter in FetchAnswerCommentsController

--- src/infra/http/controllers/fetch-answer-comments.controller.ts
+++ src/infra/http/controllers/fetch-answer-comments.controller.ts
@@ -8,7 +8,7 @@ import {
 import { ZodValidationPipe } from '@/infra/http/pipes/zod-validation-pipe';
 import { z } from 'zod';
 import { FetchAnswerCommentsUseCase } from '@/domain/forum/application/use-cases/fetch-answer-comments';
-import { CommentPresenter } from '../presenters/comment-presenters';
+import { CommentWithAuthorPresenter } from '../presenters/comment-with-author-presenter';
 
 const pageQueryParamSchema = z
   .string()
@@ -39,8 +39,8 @@ export class FetchAnswerCommentsController {
       throw new BadRequestException();
     }
 
-    const answerComments = result.value.answersComments;
+    const comments = result.value.comments;
 
-    return { comments: answerComments.map(CommentPresenter.toHttp) };
+    return { comments: comments.map(CommentWithAuthorPresenter.toHttp) };
   }
 }
