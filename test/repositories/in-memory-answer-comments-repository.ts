c5bfcc5836de4394e299ef9a728a3e8bdf2b227c
Romulo Sobreira <sobreiraromuloes@gmail.com>
2023-11-15 14:17:05 -0300

    feat: Add findManyByAnswerIdWithAuthor method to AnswersCommentRepository

--- test/repositories/in-memory-answer-comments-repository.ts
+++ test/repositories/in-memory-answer-comments-repository.ts
@@ -1,11 +1,15 @@
 import { PaginationParams } from '@/core/repositories/pagination-params';
 import { AnswersCommentRepository } from '@/domain/forum/application/repositories/answers-comments-repository';
 import { AnswerComment } from '@/domain/forum/enterprise/entities/answer-comment';
+import { CommentWithAuthor } from '@/domain/forum/enterprise/entities/value-objects/comment-with-author';
+import { InMemoryStudentRepository } from './in-memory-students-repository';
 
 export class InMemoryAnswerCommentRepository
   implements AnswersCommentRepository
 {
   public items: AnswerComment[] = [];
+
+  constructor(private studentsRepository: InMemoryStudentRepository) {}
 
   async findById(id: string) {
     const answerComment = this.items.find((item) => item.id.toString() === id);
@@ -22,6 +26,35 @@ export class InMemoryAnswerCommentRepository
     const answerComments = this.items
       .filter((item) => item.answerId.toString() === answerId)
       .slice((page - 1) * 20, page * 20);
+
+    return answerComments;
+  }
+
+  async findManyByAnswerIdWithAuthor(
+    answerId: string,
+    { page }: PaginationParams,
+  ): Promise<CommentWithAuthor[]> {
+    const answerComments = this.items
+      .filter((item) => item.answerId.toString() === answerId)
+      .slice((page - 1) * 20, page * 20)
+      .map((comment) => {
+        const author = this.studentsRepository.items.find((student) =>
+          student.id.equals(comment.authorId),
+        );
+
+        if (!author) {
+          throw new Error(`Author with id ${comment.authorId} does not exist`);
+        }
+
+        return CommentWithAuthor.create({
+          commentId: comment.id,
+          content: comment.content,
+          createdAt: comment.createdAt,
+          updatedAt: comment.updatedAt,
+          authorId: comment.authorId,
+          author: author.name,
+        });
+      });
 
     return answerComments;
   }
